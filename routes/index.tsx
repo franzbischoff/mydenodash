05da474f332c4134a9a26906f7dfca0f56812f35
Francisco Bischoff <984592+franzbischoff@users.noreply.github.com>
2023-08-29 01:04:46 +0100

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
+          Welcome to `fresha`. Try updating this message in the
           ./routes/index.tsx file, and refresh.
         </p>
         <Counter count={count} />
